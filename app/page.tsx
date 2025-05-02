530da7ef1e9f49e47844ba6b96238d44e70ba482
zegiha <leeseoyul070814@gmail.com>
2025-05-02 17:52:24 +0900

    remove: 작업물 제거

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,10 @@
 'use client'
 
 import { Col } from "@/shared/flex";
-import MainFrame from "@/widgets/MainFrame/MainFrame";
-import NewsFrame from "@/widgets/NewsFrame/NewsFrame";
-import TagFrame from "@/widgets/TagFrame/TagFrame";
-
 
 export default function Page() {
   return (
     <Col>
-      <MainFrame/>
-      <TagFrame/>
-      <NewsFrame/>
     </Col>
   )
 }
