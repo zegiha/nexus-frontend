ce28b2a346849ae646fd876aceb8f3d9b5743565
zegiha <leeseoyul070814@gmail.com>
2025-06-02 19:56:53 +0900

    refactor: delete lint warn

--- widgets/press/headlineSection/index.ts
+++ widgets/press/headlineSection/index.ts
@@ -1,9 +1,7 @@
 import HeadlinesGrid from './ui/HeadlinesGrid'
-import PressCategories from './ui/PressCategories'
 import PressPageHeadline from './ui/PressPageHeadline'
 
 export {
   HeadlinesGrid,
-  PressCategories,
   PressPageHeadline
 }
